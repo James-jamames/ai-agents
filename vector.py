bea49c826978f016f06216cde7984f5131444f04
Tiago Gonçalves <tiagogoncalves@discente.ufg.br>
2025-07-11 11:15:50 -0300

    + Vectors update

--- vector.py
+++ vector.py
@@ -7,15 +7,31 @@ from langchain_core.documents import Document
 
 df = pd.read_csv('realistic_restaurant_reviews.csv')
 
-embeddings = OllamaEmbeddings(model="mxbai-embed-large")
+embedding_function = OllamaEmbeddings(model="mxbai-embed-large")
 
-db_location = "./chroma_langchain_db"
+persist_directory = "./chroma_langchain_db"
 
-add_documents = not os.path.exists(db_location)
+already_exists = os.path.exists(persist_directory)
 
-if add_documents:
-    documents = []
+if not already_exists:
     ids = []
-
+    documents = []
+    
     for i, row in df.iterrows():
-        document = 
+        document = Document(
+            id=str(i),
+            page_content=row['title'] + " " + row["Review"],
+            metadata={"rating": row["Rating"], "date": row["Date"]}
+        )
+
+        ids.append(str(i))
+        documents.append(document)
+
+vector_store = Chroma(
+    collection_name="restaurant_reviews",
+    persist_directory=persist_directory,
+    embedding_function=embedding_function
+)
+
+if not already_exists:
+    vector_store.add_documents(documents=documents, ids=ids)
